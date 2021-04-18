5d5fd97e636b6379c99dac0cb7b531766c17b5e2
andresc <andrescamilo.rosero@gmail.com>
2021-04-18 00:57:28 -0300

    end of project

--- main.js
+++ main.js
@@ -9,4 +9,104 @@ const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
 //song titles
-const songs = ['hey','summer','ukelele'];
+const songs = ['hey','summer','ukulele'];
+
+//keep track of songs
+let songIndex = 1;
+
+//load song info into DOM
+loadSongs(songs[songIndex]);
+
+//uptade song details
+
+function loadSongs(song){
+    title.innerHTML = song;
+    audio.src = `./music/${song}.mp3`;
+    cover.src = `./images/${song}.jpg`;
+}
+
+function playSong(){
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+
+function pauseSong(){
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+}
+
+function prevSong(){
+    songIndex--;
+
+    if(songIndex < 0){
+        songIndex = songs.length -1;
+    }
+
+    loadSongs(songs[songIndex])
+    playSong()
+}
+
+function nextSong() {
+
+    songIndex++;
+
+    if(songIndex > songs.length -1){
+        songIndex = 0;
+    }
+
+    loadSongs(songs[songIndex])
+    playSong()
+}
+
+
+function updateProgressBar(e){
+    const {duration, currentTime} = e.srcElement;
+    const progressPercentage = (currentTime / duration) * 100
+    progress.style.width = `${progressPercentage}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+
+//event listeners
+playBtn.addEventListener('click', ()=> {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying){
+        pauseSong();
+    } else {
+        playSong();
+    }
+})
+
+//change song events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+audio.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgress)
+audio.addEventListener('ended', nextSong)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
